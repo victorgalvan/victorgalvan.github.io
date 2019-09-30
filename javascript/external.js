85dfdcc1f56e6bda3bf0ddb31af4af28d1efe817
Victor <v@Victors-MacBook-Air.local>
2019-09-29 21:34:14 -0500

    Replaced the header with user prompt information

--- javascript/external.js
+++ javascript/external.js
@@ -7,24 +7,27 @@
 //const out = x+y;
 //document.write(out);
 // A prompt box is used to prompt users to input a value before entering a page.
-//user_name=window.prompt("Please enter your name", "Type your name here");
+user_name=window.prompt("Please enter your name", "Type your name here");
+const x= user_name
+const out= x
+//document.write(out);
 //document.write(user_name);
 //x=22;
 //const y=33;
 //document.writeln(x+y);
 //document.writeln("<br>");
 //document.writeln(x += 33);
-document.writeln("<button onclick='condition()'>Conditional Test</button>")
+//document.writeln("<button onclick='condition()'>Conditional Test</button>")
 
-function condition()
-{
-  x=confirm("Are you sure you want to proceed?");
-  if(x)
-  {
-    document.writeln("You chose Okay!");
-  }
-  else
-  {
-    document.writeln("You chose Cancel!");
-  }
-}
+//function condition()
+//{
+//  x=confirm("Are you sure you want to proceed?");
+//  if(x)
+//  {
+//    document.writeln("You chose Okay!");
+//  }
+//  else
+//  {
+//    document.writeln("You chose Cancel!");
+//  }
+//}
